8dfce60ffd662fde4b41ae427719df1f9625d1c8
Lucas Antônio <lucasantoniobrandao008@gmail.com>
2024-03-22 05:56:08 -0300

    dando um começo

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { ref } from "vue";
+
+
+</script>
+ 
+<template>
+    <div class="conteiner">
+        <input type="text" name="" id="">
+        <label for="">nome</label>
+    </div>
+</template>
