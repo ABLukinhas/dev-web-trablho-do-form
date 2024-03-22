42ceb9c934f4bc5a5a1694b39a539418c19bc3ae
ABLukinhas <lucasantoniobrandao008@gmail.com>
2024-03-22 13:35:25 -0300

    arrumando

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,51 @@
 <script setup>
 import { ref } from "vue";
 
+const pessoa = ref({
+  nome: '',
+  sobrenome: '',
+  email: '',
+  senha: '',
+  cidade: '',
+  estado: '',
+  datanascimento: '',
+  hobbies: [],
+  preferenciadLinguagem: ''
+})
+
+
 
 </script>
  
 <template>
     <div class="conteiner">
-        <input type="text" name="" id="">
-        <label for="">nome</label>
+        <div>
+            <label for="">nome</label>
+            <input type="text" v-model="name"  id="">
+        </div>
+        <div>
+            <label for="">sobrenome</label>
+            <input type="text" v-model="sobrenome">
+        </div>
+        <div>
+            <label for="">email</label>
+            <input type="text" v-model="email">
+        </div>
+        <div>
+            <label for="">senha</label>
+            <input type="password" v-model="senha">
+        </div> 
+        <div>
+            <label for="">data de nascimento</label>
+            <input type="date" v-model="datanascimento">
+        </div>   
+        <div>
+            <label for="">hobbies</label>
+            <input type="#" v-model="hobbies">
+        </div>
+        <div>
+            <label for="">preferencia de linguagem</label>
+            <input type="#" v-model="preferenciadLinguagem">
+        </div>
     </div>
 </template>
